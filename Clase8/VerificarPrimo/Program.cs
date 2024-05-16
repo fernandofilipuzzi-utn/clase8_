7f88e9b898f91d8661ca56e1998acae0f2b8bb12
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2024-05-16 16:31:01 -0300

    ejercicio de verificar primos listo

---
+++ Clase8/VerificarPrimo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerificarPrimo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese el numero");
+
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            #region contar divisibilidades
+            int divisibilidades = 0;
+            for (int n = 1; n <= numero; n++)
+            {
+                if (numero % n == 0)
+                {
+                    divisibilidades++;
+                }
+            }
+            #endregion
+
+            #region verifico la condicion de primo e informo
+            if (divisibilidades <= 2)
+            {
+                Console.WriteLine("es primo");
+            }
+            else 
+            {
+                Console.WriteLine("no es primo");
+            }
+            #endregion
+        }
+    }
+}
