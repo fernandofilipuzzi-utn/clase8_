f35b44133d4763941ba956b9f02ac11214ca2df4
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2024-05-16 17:22:27 -0300

    modificar la solución para generar números primos

--- Clase8/GenerarPrimos/Program.cs
+++ Clase8/GenerarPrimos/Program.cs
@@ -10,6 +10,36 @@ namespace GenerarPrimos
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Solicitar desde y hasta");
+            int desde = Convert.ToInt32(Console.ReadLine());
+            int hasta = Convert.ToInt32(Console.ReadLine());
+
+            for (int m = desde; m <= hasta; m++)
+            {
+
+               
+                int numero = m;
+                #region evaluar si es primo y lo muestro si es
+             
+                int divisibilidades = 0;
+                for (int n = 1; n <= numero; n++)
+                {
+                    if (numero % n == 0)
+                    {
+                        divisibilidades++;
+                    }
+                }
+
+               
+                if (divisibilidades <= 2)
+                {
+                    Console.WriteLine("{0}", numero);
+                }
+                #endregion
+
+
+                Console.ReadKey();
+            }
         }
     }
 }
